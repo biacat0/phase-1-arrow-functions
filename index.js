c975f6bba4eba50f6da72784ac5c2d450222e181
June Biadog <jb.biadog@gmail.com>
2022-09-07 09:55:40 -0400

    commit tree d7efd8f6fb4c999cf6cc4276c1451e91ee76ad12

---
+++ index.js
@@ -0,0 +1,22 @@
+// function divide() {
+//     let num1 = 2000;
+//     let num2 = 100;
+//     return (num1/num2);
+// }
+
+// function square() {
+//     let num3 = 2;
+//     return (num3 = 46;
+// }
+
+// function add() {
+//     let num4 = 16
+//     let num5 = 56
+//     return (num4+num5)
+// }
+
+const divide = (num1, num2) => num1/num2
+
+const square = (num3) => num3*num3
+
+const add = (num4, num5) => num4+num5
